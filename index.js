551b33acff59f11b9865fd484f8e0a9fcb598d20
Furqan Safdar <furqan.safdar@gmail.com>
2023-11-15 22:13:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use((req, res, next) => {
   next();
 });
 
+/*
 app.get('/jokes/random', (req, res) => {
   request(
     { url: 'https://joke-api-strict-cors.appspot.com/jokes/random' },
@@ -17,6 +18,40 @@ app.get('/jokes/random', (req, res) => {
       }
 
       res.json(JSON.parse(body));
+    }
+  )
+});
+*/
+
+app.get('*', (req, res) => {
+  debugger;
+
+  const protocol = req.protocol;
+  //const host = req.hostname;
+  //const port = process.env.PORT || PORT;
+  var url = req.originalUrl;
+  if (url && url != '/') {    
+    url = url.substring(1, url.length);
+    console.log("Request:" + url);
+  } else {
+    console.log("Request: No URL found!");
+    return;
+  }
+  //const fullUrl = `${protocol}://${url}`;
+  const fullUrl = url.replace(":/", "://");
+
+  request(
+    //{ url: 'https://joke-api-strict-cors.appspot.com/jokes/random' },
+    { url: fullUrl },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: error.message });
+      }
+
+      //console.log(body);
+
+      //res.json(JSON.parse(body));
+      res.send(body);
     }
   )
 });
